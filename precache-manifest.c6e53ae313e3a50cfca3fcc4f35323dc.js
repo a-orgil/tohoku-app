20faa90bc3c51535b3dcab2366e14e8a8c42f0b4
a-orgil <aorgil@otas-kk.co.jp>
2022-12-06 13:23:20 +0900

    Updates

---
+++ precache-manifest.c6e53ae313e3a50cfca3fcc4f35323dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "35933847975fe2fdde09",
+    "url": "/tohoku-app/static/css/main.8316ecb8.chunk.css"
+  },
+  {
+    "revision": "35933847975fe2fdde09",
+    "url": "/tohoku-app/static/js/main.35933847.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/tohoku-app/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "fe9e49dcc9c458a203f8",
+    "url": "/tohoku-app/static/js/2.fe9e49dc.chunk.js"
+  },
+  {
+    "revision": "15bc068878faa7840702",
+    "url": "/tohoku-app/static/js/runtime~main.15bc0688.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tohoku-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4ad4f427b466cc43f232dc5e429623a5",
+    "url": "/tohoku-app/index.html"
+  }
+];
